79b131b9195f024b73bd87e5edafcc594c884e98
MH Shuvo <mhshuvoalways@gmail.com>
2024-08-07 08:49:23 +0600

    page bg change

--- tools-selling-client/app/components/hero/index.jsx
+++ tools-selling-client/app/components/hero/index.jsx
@@ -1,27 +1,8 @@
+import BgImg from "@/public/images/toolsbg.png";
+import Image from "next/image";
+
 const Hero = () => {
-  return (
-    <div
-      style={{
-        backgroundImage: "url(/images/toolsbg.png)",
-      }}
-      className="h-[50vh] bg-no-repeat bg-center bg-cover flex justify-center items-center"
-    >
-      <div className="bg-white rounded-xl px-5 sm:px-20 py-5 sm:py-10 flex justify-center items-center text-center mainWidht sm:w-6/12 shadow">
-        <div className="space-y-5">
-          <p className="text-primary text-5xl font-bold">SINC</p>
-          <p className="text-4xl font-semibold">
-            ALL YOU NEED FOR YOUR NEXT DIY
-          </p>
-          <div className="flex items-center gap-2 justify-center text-xl">
-            <i className="fa-solid fa-location-dot"></i>
-            <p className="font-semibold">
-              Ha-Yozma St 1, Rishon Letshiyon
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <Image src={BgImg} alt="" className="w-full" />;
 };
 
 export default Hero;
