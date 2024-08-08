49c038695567a232cbe8c945be9ebe6669403967
MH Shuvo <mhshuvoalways@gmail.com>
2024-08-08 09:55:27 +0600

    refactor: refactor

--- tools-selling-client/tailwind.config.js
+++ tools-selling-client/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#36454F",
+        primary: "#000000",
       },
     },
   },
--- tools-selling-dashboard/tailwind.config.js
+++ tools-selling-dashboard/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#36454F",
+        primary: "#000000",
       },
     },
   },
