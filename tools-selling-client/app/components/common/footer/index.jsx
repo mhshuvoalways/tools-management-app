d920f6630b2f238b9f05c8db84253b739c61f689
MH Shuvo <mhshuvoalways@gmail.com>
2024-08-08 09:53:45 +0600

    refactor: refactor

--- tools-selling-client/app/components/common/footer/index.jsx
+++ tools-selling-client/app/components/common/footer/index.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const index = () => {
   return (
-    <div className="bg-primary py-20 absolute -bottom-60 left-0 right-0">
-      <div className=" text-white flex justify-between gap-5 mainWidht flex-wrap">
+    <div className="bg-red-600 h-40 py-5 absolute -bottom-60 left-0 right-0">
+      <div className=" text-white mainWidht">
         <p className="text-3xl font-bold">
           <Link href={"/"}>SINC</Link>
         </p>
